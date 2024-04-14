82fd3d5865b319ff8f8bbd8015648fc90f9089ea
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-04-14 22:26:26 +0600

    end

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
+import { GithubAuthProvider } from 'firebase/auth/cordova';
 const Login = () => {
     const [user, setUser] = useState(null);
 
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handelGoogleSignIn = () => {
-        
-        signInWithPopup(auth, provider)
+
+        signInWithPopup(auth, googleProvider)
             .then(result => {
                 const loggedInUser = result.user;
                 console.log(loggedInUser);
+                setUser(loggedInUser)
             })
             .catch(error => {
                 console.log('error', error.message)
             })
 
+    }
+
+    const handelGithubSignIn = () => {
+        signInWithPopup (auth, githubProvider)
+        .then ( result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setUser(loggedUser);
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 
     const handelSignOut = () => {
@@ -33,10 +48,12 @@ const Login = () => {
 
     return (
         <div>
-            { user ?
+            {user ?
                 <button onClick={handelSignOut}>SignOut</button> :
-                <button onClick={handelGoogleSignIn}>Google login</button>
-                
+                <>
+                    <button onClick={handelGoogleSignIn}>Google login</button>
+                    <button onClick={handelGithubSignIn}>Github Login</button>
+                </>
             }
 
             {
